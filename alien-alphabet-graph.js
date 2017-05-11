e1170446beac9ba0655c83bb10dfedb7c33003f1
brials <brials008@hotmail.com>
2017-05-10 19:00:30 -0700

    commiting old stuff to continue working

--- alien-alphabet-graph.js
+++ alien-alphabet-graph.js
@@ -2,15 +2,27 @@
 
 //implement a function that determines a possible orderinng for an alien alphabet given an array of words from the language
 
+function Node(name){
+  this.name = name;
+  this.in = [];
+  this.inNum = 0;
+  this.out = [];
+  this.outNum = 0;
+}
+
+let graph = [];
+
+
 function createRules(words){
   let rules = [];
-  let unique = [];
   for(let rI = 0; rI < words.length; rI++){
     let foundDif = false;
     for(let lI = 0; lI < words[rI].length; lI++){
-      if(unique.indexOf(words[rI][lI]) < 0){
-        unique.push(words[rI][lI])
+      let foundLet = false;
+      for(let i = 0; i < graph.length; i++){
+        if(graph[i].name === words[rI][lI]) foundLet = true;
       }
+      if(!foundLet) graph.push(new Node(words[rI][lI]));
       if(words[rI][lI] && words[rI + 1] && words[rI + 1][lI]){
         if(words[rI][lI] != words[rI + 1][lI] && !foundDif){
           rules.push([words[rI][lI], words[rI + 1][lI]]);
@@ -21,16 +33,16 @@ function createRules(words){
   }
 
   console.log('rules', rules);
-  for(let i = 0; i< rules.length; i++){
-    let leftI = unique.indexOf(rules[i][0]);
-    let rightI = unique.indexOf(rules[i][1]);
-    if(leftI > rightI){
-      unique.splice(rightI, 0, unique.splice(leftI, 1)[0]);
-    }
-  }
-
-  return unique;
-}
+  // for(let i = 0; i< rules.length; i++){
+  //   let leftI = unique.indexOf(rules[i][0]);
+  //   let rightI = unique.indexOf(rules[i][1]);
+  //   if(leftI > rightI){
+  //     unique.splice(rightI, 0, unique.splice(leftI, 1)[0]);
+  //   }
+  // }
+  // 
+  //   return unique;
+  // }
 
 let test = ['baa', 'abcd', 'abca', 'cab', 'cad'];
 let test2 = ['xza', 'ayh', 'ples', 'plares', 'bhaaz', 'bnc'];
