9c1201635f1697b26b991ae573a3486fce09de05
brials <brials008@hotmail.com>
2017-08-14 13:37:27 -0700

    added iter fib

--- fibonacci.js
+++ fibonacci.js
@@ -18,3 +18,27 @@ console.log(calcNFibonacci(8));
 console.log(calcNFibonacci(9));
 console.log(calcNFibonacci(10));
 console.log(calcNFibonacci(11));
+
+function calcNFibIter(n){
+  if(!n) return 'please enter a number';
+  if(n === 1) return 0;
+  if(n === 2) return 1;
+  let first = 0;
+  let second = 1;
+  let count = 2;
+  while(count < n){
+    let current = first + second;
+    first = second;
+    second = current;
+    count++;
+  }
+  return second;
+}
+
+console.log(calcNFibIter(3));
+console.log(calcNFibIter(4));
+console.log(calcNFibIter(5));
+console.log(calcNFibIter(6));
+console.log(calcNFibIter(7));
+console.log(calcNFibIter(8));
+console.log(calcNFibIter(9));
